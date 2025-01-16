03303be0a84727dcff413d5e2e36153dc6d7c571
dianamatata <diana.avalos.m@gmail.com>
2025-01-16 09:38:20 +0100

    change paths
    update paths
    remove last_processed_line file handling cause not used

--- extract_mobidetails_info.py
+++ extract_mobidetails_info.py
@@ -71,26 +71,16 @@ def extract_data_from_website(driver):
     return gene_name, tdf
 
 # File paths ------------------
-formatted_entries_file = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/formatted_vcf_entries.txt"
-failed_variants_log = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/failed_variants_log.txt"
+formatted_entries_file = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/data/formatted_vcf_entries.txt"
+failed_variants_log = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/out_mobi/failed_variants_log.txt"
 
 # for second iteration, once we had some fails with failed_variants_log, was it timeout or was it failure to identify transcript?
-formatted_entries_file = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/failed_variants_log_hg38.txt"
-failed_variants_log = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/failed_variants_log2.txt"
+formatted_entries_file = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/out_mobi/failed_variants_log_hg38.txt"
+failed_variants_log = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/out_mobi/failed_variants_log2.txt"
 
 # main ------------------
 
-# Track the last processed line
-last_processed_line_file = '/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/last_processed_line.txt'
-output_mobidetails = '/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/output/output_mobidetails'
-
-# Check if the file exists, and if so, read the last processed line
-try:
-    with open(last_processed_line_file, 'r') as f:
-        last_processed_line = int(f.read().strip())
-except FileNotFoundError:
-    last_processed_line = 0  # If the file does not exist, start from the beginning
-
+output_mobidetails = '/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/out_mobi/output_mobidetails'
 
 # Initialize the webdriver
 driver = webdriver.Chrome(options=chrome_options)
@@ -107,10 +97,10 @@ with open(formatted_entries_file, 'r') as f:
 
 # Initialize the iteration counter and tdf1
 tdf1 = pd.DataFrame()
-
-last_processed_line = line_num -1
-last_processed_line= 0
-iteration = 52
+last_processed_line = 0
+iteration = 1
+# for debug: last_processed_line = line_num -1
+# for debug: iteration = 52
 
 # Read the formatted VCF entries from the file
 with open(formatted_entries_file, 'r') as f:
@@ -168,27 +158,22 @@ with open(formatted_entries_file, 'r') as f:
             with open(failed_variants_log, 'a') as log_file:
                 log_file.write(f"Failed to concatenate variant {variant} on line {line_num}\n")
             print(f"Variant {variant} failed to concatenate on line {line_num}. Skipping to next.")
-            continue
 
         if line_num % 50 == 0:  # Check if the number of rows is a multiple of 50, to not write all the time
             output_filename = f"{output_mobidetails}_{iteration}.txt"
+            print("saving output")
             tdf1.to_csv(output_filename, index=False)
             print(f"Saved to {output_filename}")
             tdf1 = pd.DataFrame() # reinitialize
             iteration += 1
             last_processed_line = line_num
-            with open(last_processed_line_file, 'w') as f:
-                f.write(str(last_processed_line))
 
-tdf1.shape
 # Close the driver
-# driver.quit()
+driver.quit()
 
 # check
-output_filename = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/output_mobidetails_2.txt"
-df = pd.read_csv(output_filename)
-print(df.shape)
-print(df.head())
+# output_filename = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/output_mobidetails_2.txt"
+# df = pd.read_csv(output_filename)
+# print(df.shape)
+# print(df.head())
 
-# Close the browser
-driver.quit()
