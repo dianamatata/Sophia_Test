e72cfc7e52fe232e71c55fed95618111691713e0
dianamatata <diana.avalos.m@gmail.com>
2025-01-21 13:16:39 +0100

    clean

--- get_clinvar_data.sh
+++ get_clinvar_data.sh
@@ -49,35 +49,3 @@ awk -F'\t' 'BEGIN { OFS="\t" }
 NR == 1 { print $0, "hg38_vcf_clinvar" }  # Add new column header
 NR > 1 { print $0, "hg38:" $1 ":" $2 ":" $4 ":" $5 }'  > /Users/dianaavalos/Desktop/Tertiary_Research_Assignment/data/summary_variants_clinvar.txt
 
-#clinvar_gene_data_output_file = "/Users/dianaavalos/Desktop/Tertiary_Research_Assignment/data/SAMD11_variants.txt"
-#clinvar_gene_data = pd.read_csv(clinvar_gene_data_output_file, sep='\t')
-
-
-# for each variant,take hotspot on +25 and -25:
-# check hoy many types of variants in each
-# best is to extract +100/-100 from chr, pos
-
-
-
-
-
-#  other stuff -------------------
-
-#while read -r line; do
-#  # Extract the 5th field from each line using 'cut' and append it to the output file
-#  echo "$line" | cut -d " " -f5 >> /Users/dianaavalos/Desktop/Tertiary_Research_Assignment/failed_variants_log_hg38.txt
-#done < /Users/dianaavalos/Desktop/Tertiary_Research_Assignment/failed_variants_log.txt
-
-# TODO understand why it does not work with "$failed_variants_log"
-## extract variants that did not work
-#failed_variants_log="/Users/dianaavalos/Desktop/failed_variants_log.txt"
-#failed_variants_log2="/Users/dianaavalos/Desktop/failed_variants_log2.txt"
-#
-## Clear the output file before writing to it
-#> "$failed_variants_log2"
-#
-## Process each line and extract the 5th field
-#while read -r line; do
-#  # Extract the 5th field from each line using 'cut' and append it to the output file
-#  echo "$line" | cut -d " " -f5 >> "$failed_variants_log2"
-#done < "$failed_variants_log"
